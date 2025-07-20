f43df5574958579026b5546b6aa003177bcb58af
FluffyRudy <192571480+FluffyBrudy@users.noreply.github.com>
2025-07-20 21:45:13 +0545

    remove logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,7 @@
 import { Module } from '@nestjs/common';
 import { YoutubeModule } from './youtube/youtube.module';
-import { LoggerModule } from 'nestjs-pino';
-import { pinoLoggerConfig } from './pino.logger.config';
 
 @Module({
-  imports: [
-    YoutubeModule,
-    ...(process.env.NODE_ENV === 'dev'
-      ? [LoggerModule.forRoot(pinoLoggerConfig)]
-      : []),
-  ],
+  imports: [YoutubeModule],
 })
 export class AppModule {}
